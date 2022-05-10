333d2ce50a3f7a9b0ca59f708d444597692b3f68
Leon Si <contact@leonzalion.com>
2022-05-10 11:48:33 -0400

    feat: update cli

--- packages/print-double-sided/src/bin/cli.ts
+++ packages/print-double-sided/src/bin/cli.ts
@@ -1,3 +1,5 @@
+import { program } from 'commander';
+import { execaSync } from 'execa';
 import inquirer from 'inquirer';
 import PressToContinue from 'inquirer-press-to-continue';
 import { nanoid } from 'nanoid-nice';
@@ -12,6 +14,23 @@ import { openPrintMenu } from '~/utils/print-menu.js';
 import { selectPrintOddEvenPages } from '~/utils/print-panel.js';
 
 inquirer.registerPrompt('press-to-continue', PressToContinue);
+
+program
+	.showHelpAfterError()
+	.argument('<input-file>', 'the path to the file to print')
+	.parse();
+
+const filePath = program.args[0]!;
+execaSync('open', [filePath]);
+
+await openPrintMenu();
+await inquirer.prompt({
+	type: 'press-to-continue',
+	name: 'key',
+	key: 'c',
+	pressToContinueMessage:
+		'Customize the print settings until you are happy with them (but do NOT press Print!), then press `c` to continue...',
+});
 
 const temporaryPresetName = nanoid();
 await saveCurrentSettingsAsPreset({ presetName: temporaryPresetName });
